a5adbebd31ea1ad6c69cc45dcb1fe35a326bfcdf
smalaca <ab13.krakow@gmail.com>
2021-12-13 11:43:23 +0100

    Initial commit

---
+++ src/main/java/com/creativespace/trends/TrendsApplication.java
@@ -0,0 +1,12 @@
+package com.creativespace.trends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor")
+public class TrendsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrendsApplication.class, args);
+    }
+}
