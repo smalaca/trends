dd19e8660080097711f6f5cee16db0df53a5f1b8
smalaca <ab13.krakow@gmail.com>
2021-12-13 12:19:10 +0100

    introduce TrendsClient to hide MvcMock usage

--- src/test/java/com/creativespace/trends/adapter/web/api/rest/TrendsRestControllerTest.java
+++ src/test/java/com/creativespace/trends/adapter/web/api/rest/TrendsRestControllerTest.java
@@ -1,13 +1,14 @@
 package com.creativespace.trends.adapter.web.api.rest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,17 +18,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class TrendsRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    private TrendsClient client;
+
+    @BeforeEach
+    void setUpTrendsClient() {
+        client = new TrendsClient(mockMvc);
+    }
 
     @Test
     void shouldSayHelloToSpecificPerson() throws Exception {
-        mockMvc.perform(get("/hello-world").param("name", "Pater Parker"))
+        ResultActions actual = client.helloWorld("Pater Parker");
+
+        actual
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello Pater Parker! Are you looking for some suggestions?"));
     }
 
     @Test
     void shouldSayHelloToNoName() throws Exception {
-        mockMvc.perform(get("/hello-world"))
+        ResultActions actual = client.helloWorld();
+
+        actual
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello No Name! Are you looking for some suggestions?"));
     }
